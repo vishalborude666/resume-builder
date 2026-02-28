8fefc02fdf89220b57650e4885d17f5f9945102a
vishalborude666 <167678163+vishalborude666@users.noreply.github.com>
2026-02-28 19:06:02 +0530

    Updated project

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,11 +1,8 @@
 import axios from 'axios'
 
-const rawBase = import.meta.env.VITE_BASE_URL;
-// strip surrounding quotes if present and provide a local fallback
-const baseURL = (rawBase || '').replace(/^"|"$/g, '') || 'http://localhost:3000';
 
-const api = axios.create({
-   baseURL
-})
+const api = import.meta.env.VITE_API_URL;
+
+axios.post(`${api}/api/users/register`, data)
 
 export default api;
