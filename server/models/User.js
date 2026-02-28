b0e3aae9e52530b05b56d99e96227c9fe9252fd6
vishalborude666 <167678163+vishalborude666@users.noreply.github.com>
2026-02-28 19:23:25 +0530

    Fixed User model import issue

--- server/models/User.js
+++ server/models/User.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-import User from "/models/User.js";
-const UserSchema = new mongoose.Schema({
-    name : {type:String, required:  true},
-    email: {type:String, required: true, unique: true},
-    password: {type:String, required: true},}
-    ,{timestamps: true}
-)
+const UserSchema = new mongoose.Schema(
+{
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+},
+{ timestamps: true }
+);
 
 UserSchema.methods.comparePassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-}
-const User = mongoose.model("User", UserSchema)
+  return bcrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
+
 export default User;
